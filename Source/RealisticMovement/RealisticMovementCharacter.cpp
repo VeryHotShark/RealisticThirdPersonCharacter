2243f0d8ca4e3cb1e50bc7115fe0cd26914e4fab
VeryHotShark <nguyen.tu.tony2000@gmail.com>
2023-02-28 18:19:28 +0100

    Camera Changed

--- Source/RealisticMovement/RealisticMovementCharacter.cpp
+++ Source/RealisticMovement/RealisticMovementCharacter.cpp
@@ -40,7 +40,7 @@ ARealisticMovementCharacter::ARealisticMovementCharacter()
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	CameraBoom->bUsePawnControlRotation = true; //  Rotate the arm based on the controller
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
